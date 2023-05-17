2995c84a965ca20a7be09aa18f18a5572358db34
NHIRANJAN <116147848+NHIRANJAN@users.noreply.github.com>
2023-05-17 19:35:30 +0530

    Delete do it directory

--- do it/js.js
+++
@@ -1,9 +0,0 @@
-function login(){
-    document.getElementById("log").style.display="block"
-    document.getElementById("reg").style.display="none"
-}
-function register(){
-    document.getElementById("reg").style.display="block"
-    document.getElementById("log").style.display="none"
-    
-}
