48d37fe539234722446af062aa7724e3a621c0f2
NHIRANJAN <116147848+NHIRANJAN@users.noreply.github.com>
2023-05-17 19:36:01 +0530

    Add files via upload

---
+++ js.js
@@ -0,0 +1,9 @@
+function login(){
+    document.getElementById("log").style.display="block"
+    document.getElementById("reg").style.display="none"
+}
+function register(){
+    document.getElementById("reg").style.display="block"
+    document.getElementById("log").style.display="none"
+    
+}
